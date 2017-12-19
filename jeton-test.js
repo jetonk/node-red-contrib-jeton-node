cd1e775694c8ca2997cbcf6b1b04e91501fe7675
Jeton <jeton.kadrija@gmail.com>
2017-12-19 21:09:54 +0100

    Parse

--- jeton-test.js
+++ jeton-test.js
@@ -5,10 +5,10 @@ module.exports = function(RED) {
         node.on('input', function(msg) {
             var total = 0;
             if(msg.number !== "" || msg.number !== undefined){
-                var parsed = msg.number;
+                var parsed = parseInt(msg.number);
                 total = (parsed * parsed) + 3;
-                // msg.payload = '(' + parsed + ' * ' + parsed + ')+ 3 = '+ total;
                 msg.payload = `(${parsed} * ${parsed}) +3 = ${total}`;
+                node.status(msg);
                 node.send(msg);
             }else{
                 node.send(msg);
