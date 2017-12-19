650b4dfdba6a6e73f83138b57dfea26502f43d17
Jeton <jeton.kadrija@gmail.com>
2017-12-19 18:48:08 +0100

    Changed to fetch params

--- jeton-test.js
+++ jeton-test.js
@@ -3,9 +3,8 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         var node = this;
         node.on('input', function(msg) {
-            node.log(msg);
             node.log(msg.payload);
-            node.send(msg);
+            node.send(msg.req.query.number);
         });
     }
     RED.nodes.registerType("jeton-test",Calc);
