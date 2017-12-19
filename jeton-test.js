756f27f6063f3cb9101d19c034a203c23b638516
Jeton <jeton.kadrija@gmail.com>
2017-12-19 19:24:00 +0100

    respo change

--- jeton-test.js
+++ jeton-test.js
@@ -5,11 +5,9 @@ module.exports = function(RED) {
         node.on('input', function(msg) {
             var total = 0;
             var number = msg.number;
-            if(number !== "" || number !== undefined){
                 var parsed = parseInt(number);
                 total = (parsed * parsed) + 3;
                 node.send(total);
-            }
         });
     }
     RED.nodes.registerType("jeton-test",Calc);
