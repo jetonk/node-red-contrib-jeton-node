d38b9d80c91c8d2507bdcdd729098d8bc0d6da81
Jeton <jeton.kadrija@gmail.com>
2017-12-20 13:42:24 +0100

    Read value from config file

--- jeton-test.js
+++ jeton-test.js
@@ -1,14 +1,22 @@
 module.exports = function(RED) {
     function Calc(config) {
         RED.nodes.createNode(this,config);
+        this.number = config.number;
+        this.amount = config.amount;
         var node = this;
         node.on('input', function(msg) {
             var total = 0;
-            var parsed = parseInt(msg.payload.number);
-            total = (parsed * parsed) + 3;
-            msg.payload = '(' + parsed + ' * ' + parsed + ') + 3 = '+ total;
-            this.status({fill:"red",shape:"ring",text: JSON.stringify({parsed: parsed, total: total})});
-            node.send(msg);
+            if(msg.payload.number !== "" || msg.payload.number !== undefined){
+                var parsed = parseInt(msg.payload.number);
+                total = (parsed * parsed) + this.amount;
+                msg.payload = `(${parsed} * ${parsed}) + ${this.amount} = ${total}`;
+                this.status({fill:"red",shape:"ring",text: JSON.stringify({parsed: parsed, total: total})});
+                node.send(msg);
+            }else{
+                total = (parsed * parsed) + this.amount;
+                msg.payload = `(${this.number} * ${this.number}) + ${this.amount} = ${total}`;
+                node.send(msg);
+            }
         });
     }
     RED.nodes.registerType("jeton-test", Calc);
