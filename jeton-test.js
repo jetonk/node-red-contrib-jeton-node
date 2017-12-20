14a586526c827dde56e01490566fcfb17fdf280f
Jeton <jeton.kadrija@gmail.com>
2017-12-20 14:42:16 +0100

    Test

--- jeton-test.js
+++ jeton-test.js
@@ -6,12 +6,12 @@ module.exports = function(RED) {
         var node = this;
         node.on('input', function(msg) {
             var total = 0;
-            this.status({fill:"red",shape:"ring",text: JSON.stringify({msg: msg, payload: msg.payload})});
                 var parsed = parseInt(msg.payload.number);
                 total = (parsed * parsed) + this.amount;
-                msg.payload = '(' + parsed + ' * ' + parsed + ') + 3 = '+ total;
+                msg.payload = '(' + parsed + ' * ' + parsed + ') +'+this.amount+'+ = '+ total;
                 this.status({fill:"red",shape:"ring",text: JSON.stringify({parsed: parsed, total: total})});
                 node.send(msg);
         });
+    }
     RED.nodes.registerType("jeton-test", Calc);
 };
