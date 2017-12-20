4a99be82b138496777028f0a43a31078a45ce17f
Jeton <jeton.kadrija@gmail.com>
2017-12-20 15:00:50 +0100

    get value from settings

--- jeton-test.js
+++ jeton-test.js
@@ -7,12 +7,15 @@ module.exports = function(RED) {
         node.on('input', function(msg) {
             var total = 0;
             var parsed = parseInt(msg.payload.number);
-            this.status({fill:"red",shape:"ring",text: JSON.stringify({payload: msg.payload})});
-
-            total = (parsed * parsed) + this.amount;
+            this.status({fill:"red",shape:"ring",text: JSON.stringify({num: msg.payload.number, msg: msg.payload})});
+            if(msg.payload.number !== ""){
+                total = (parsed * parsed) + this.amount;
                 msg.payload = '(' + parsed + ' * ' + parsed + ') +' +this.amount+ ' = '+ total;
-                this.status({fill:"red",shape:"ring",text: JSON.stringify({parsed: parsed, total: total})});
                 node.send(msg);
+            }else{
+                msg.payload = '(' + this.number + ' * ' + this.number + ') +' +this.amount+ ' = '+ total;
+                node.send(msg);
+            }
         });
     }
     RED.nodes.registerType("jeton-test", Calc);
